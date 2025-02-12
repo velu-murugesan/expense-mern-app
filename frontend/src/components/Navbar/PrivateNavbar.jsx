117ccf06ab7b0ce99e9c792976ac87cf6978f2fe
velu-murugesan <velu2092002@gmail.com>
2025-02-12 10:01:46 +0530

    changes

--- frontend/src/components/Navbar/PrivateNavbar.jsx
+++ frontend/src/components/Navbar/PrivateNavbar.jsx
@@ -4,7 +4,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { IoLogOutOutline } from "react-icons/io5";
-import { SiAuthy } from "react-icons/si";
+import { GiExpense } from "react-icons/gi";
 import { logoutAction } from "../../redux/slice/authSlice";
 import React from "react";
 
@@ -43,14 +43,14 @@ export default function PrivateNavbar() {
                 </div>
                 <div className="flex flex-shrink-0 items-center">
                   {/* Logo */}
-                  <SiAuthy className="h-8 w-auto text-green-500" />
+                  <GiExpense className="h-8 w-auto text-green-500" />
                 </div>
                 <div className="hidden md:ml-6 md:flex md:space-x-8">
                   <Link
                     to="/"
                     className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
                   >
-                    MasyncTracker
+                    Home
                   </Link>
                 </div>
                 <div className="hidden md:ml-6 md:flex md:space-x-8">
@@ -157,7 +157,7 @@ export default function PrivateNavbar() {
                   as="button"
                   className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
                 >
-                  MasyncTracker
+                  Home
                 </Disclosure.Button>
               </Link>
               <Link to="/add-transaction">
