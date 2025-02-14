074fb37c554326271663e492025419f49e23f7ee
velu-murugesan <velu2092002@gmail.com>
2025-02-14 22:33:46 +0530

    Changes

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://expense-mern-app-bvb3.onrender.com/api/v1";
 
 //"https://expense-mern-app-bvb3.onrender.com/api/v1"
